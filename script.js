260ed285bbb9536caf0998c89db2f395ff0ca18d
j1cache2 <jluis7820@gmail.com>
2023-03-20 21:29:18 -0300

    Update

--- script.js
+++ script.js
@@ -81,20 +81,96 @@
 //     elementsImg.style.transition = " all 2s";
 // }
 
-// var container = document.getElementById('container');
-var container = document.getElementById('1');
-var windowHeight = window.innerHeight;
-var windowWidth = window.innerWidth;
-// var windowWidth = '4rem';
-var scrollArea = 1000 - windowHeight;
-var square1 = document.getElementById('2');
+// // var container = document.getElementById('container');
+// var container = document.getElementById('1');
+// var windowHeight = window.innerHeight;
+// var windowWidth = window.innerWidth;
+// // var windowWidth = '4rem';
+// var scrollArea = 1000 - windowHeight;
+// var square1 = document.getElementById('2');
 
-// update position of square 1 and square 2 when scroll event fires.
-window.addEventListener('scroll', function () {
-    var scrollTop = window.pageYOffset || window.scrollTop;
-    var scrollPercent = scrollTop / scrollArea || 0;
+// // update position of square 1 and square 2 when scroll event fires.
+// window.addEventListener('scroll', function () {
+//     var scrollTop = window.pageYOffset || window.scrollTop;
+//     var scrollPercent = scrollTop / scrollArea || 0;
 
-    square1.style.top = scrollPercent * window.innerWidth + 'px';
-    // square1.style.top = window.innerWidth + 'px';
-    // square1.style.top = 4+ 'rem';
+//     square1.style.top = scrollPercent * window.innerWidth + 'px';
+//     // square1.style.top = window.innerWidth + 'px';
+//     // square1.style.top = 4+ 'rem';
+// });
+
+// // seleciona o elemento que será movido
+// const elemento = document.getElementById("2");
+
+// // adiciona um evento de rolagem na página
+// window.addEventListener("scroll", function () {
+//     // obtém a quantidade de rolagem na vertical
+//     const scrollVertical = window.scrollY;
+
+//     // calcula a nova posição do elemento
+//     const novaPosicao = scrollVertical / 4;
+
+//     // utiliza requestAnimationFrame para criar uma animação suave
+//     requestAnimationFrame(function () {
+//         elemento.style.transform = `translateY(${novaPosicao}rem)`;
+//     });
+// });
+
+
+// const elemento = document.querySelector('.elementsimg');
+
+// // Adicione um listener ao evento de rolagem da página
+// window.addEventListener('scroll', () => {
+//   // Obtenha a quantidade de rolagem atual da página em pixels
+//   const scrollTop = window.pageYOffset;
+
+//   // Converta a quantidade de rolagem para rem
+//   const scrollRem = scrollTop / parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+//   // Calcule a quantidade de deslocamento horizontal que deseja aplicar
+//   const deslocamentoRem = Math.min(4, scrollRem);
+
+//   // Aplique o deslocamento horizontal ao elemento
+//   elemento.style.transform = `translateY(${deslocamentoRem}rem)`;
+// });
+
+
+// 5074
+
+// 6092
+
+// Selecione o elemento que deseja mover
+const elemento = document.querySelector('.elementsimg');
+
+// Defina as constantes para os valores de início e fim do evento
+// const inicioEvento = 5000;
+// const fimEvento = 6000;
+const inicioEvento = 5000;
+const fimEvento = 6000;
+
+// Adicione um listener ao evento de rolagem da página
+window.addEventListener('scroll', () => {
+    // Obtenha a quantidade de rolagem atual da página em pixels
+    const scrollTop = window.pageYOffset;
+
+    // Verifique se a quantidade de rolagem está dentro do intervalo do evento
+    if (scrollTop >= inicioEvento && scrollTop <= fimEvento) {
+        // Calcule a distância em pixels do topo do evento até a posição atual de rolagem
+        const distanciaEvento = scrollTop - inicioEvento;
+
+        // Calcule a quantidade de deslocamento vertical que deseja aplicar
+        const deslocamentoRem = Math.min(4, distanciaEvento / 250);
+
+        // Determine a direção da rolagem da página
+        const direcaoRolagem = scrollTop > window._ultimaPosicaoScrollTop ? 'paraCima' : 'paraBaixo';
+        window._ultimaPosicaoScrollTop = scrollTop;
+
+        // Inverta a direção da rolagem se o elemento estiver fora do eixo X da tela
+        const { top, bottom } = elemento.getBoundingClientRect();
+        const estaForaDoEixoX = top > 0 || bottom < window.innerHeight;
+        const direcaoMovimento = estaForaDoEixoX ? (direcaoRolagem === 'paraCima' ? 'paraBaixo' : 'paraCima') : direcaoRolagem;
+
+        // Aplique o deslocamento vertical ao elemento
+        elemento.style.transform = `translateY(${deslocamentoRem * (direcaoMovimento === 'paraCima' ? -1 : 1)}rem)`;
+    }
 });
---
+++ test.js
@@ -0,0 +1,222 @@
+
+"title": "section_07 parallax",
+    "continuousParameterGroups": [{
+        "id": "a-36-p",
+        "type": "SCROLL_PROGRESS",
+        "parameterLabel": "Scroll",
+        "continuousActionGroups": [{
+            "keyframe": 0,
+            "actionItems": [{
+                "id": "a-36-n",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".image-9",
+                        "selectorGuids": ["3e403f6f-a225-f707-84f2-a37965fd569a"]
+                    },
+                    "yValue": 0,
+                    "xUnit": "PX",
+                    "yUnit": "rem",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-3",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-recursos",
+                        "selectorGuids": ["29819d62-2980-2d75-8f80-daccb9433174"]
+                    },
+                    "xValue": 0,
+                    "xUnit": "rem",
+                    "yUnit": "PX",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-5",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-aulas",
+                        "selectorGuids": ["c993a694-cf26-e604-7572-ac05882d5973"]
+                    },
+                    "xValue": 0,
+                    "xUnit": "rem",
+                    "yUnit": "PX",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-7",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-modulos",
+                        "selectorGuids": ["72dc8e62-57d8-d6fc-c161-b55c6cfb5a97"]
+                    },
+                    "xValue": 0,
+                    "xUnit": "rem",
+                    "yUnit": "PX",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-9",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-atualizacoes",
+                        "selectorGuids": ["502efe56-a643-2156-e01a-70b560a973a3"]
+                    },
+                    "xValue": 0,
+                    "yValue": 0,
+                    "xUnit": "rem",
+                    "yUnit": "rem",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-11",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-alunos",
+                        "selectorGuids": ["7b305376-f34a-3955-5142-724b3c65b7a0"]
+                    },
+                    "xValue": 0,
+                    "yValue": 0,
+                    "xUnit": "rem",
+                    "yUnit": "rem",
+                    "zUnit": "PX"
+                }
+            }]
+        }, {
+            "keyframe": 100,
+            "actionItems": [{
+                "id": "a-36-n-2",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".image-9",
+                        "selectorGuids": ["3e403f6f-a225-f707-84f2-a37965fd569a"]
+                    },
+                    "yValue": 4,
+                    "xUnit": "PX",
+                    "yUnit": "rem",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-4",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-recursos",
+                        "selectorGuids": ["29819d62-2980-2d75-8f80-daccb9433174"]
+                    },
+                    "xValue": 5,
+                    "xUnit": "rem",
+                    "yUnit": "PX",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-6",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-aulas",
+                        "selectorGuids": ["c993a694-cf26-e604-7572-ac05882d5973"]
+                    },
+                    "xValue": -5,
+                    "xUnit": "rem",
+                    "yUnit": "PX",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-8",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-modulos",
+                        "selectorGuids": ["72dc8e62-57d8-d6fc-c161-b55c6cfb5a97"]
+                    },
+                    "xValue": 3,
+                    "xUnit": "rem",
+                    "yUnit": "PX",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-10",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-atualizacoes",
+                        "selectorGuids": ["502efe56-a643-2156-e01a-70b560a973a3"]
+                    },
+                    "xValue": 0,
+                    "yValue": -3,
+                    "xUnit": "rem",
+                    "yUnit": "rem",
+                    "zUnit": "PX"
+                }
+            }, {
+                "id": "a-36-n-12",
+                "actionTypeId": "TRANSFORM_MOVE",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": "._07_image-alunos",
+                        "selectorGuids": ["7b305376-f34a-3955-5142-724b3c65b7a0"]
+                    },
+                    "xValue": -3,
+                    "yValue": 0,
+                    "xUnit": "rem",
+                    "yUnit": "rem",
+                    "zUnit": "PX"
+                }
+            }]
+        }]
+    }],
