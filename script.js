8754e70d790f46e74fddffd01d978ac3ef118da3
j1cache2 <jluis7820@gmail.com>
2023-03-28 23:47:59 -0300

    Update

--- script.js
+++ script.js
@@ -84,26 +84,6 @@ function transformSpanAuto() {
 
 //Corrigir grip-column-gap para column-gap!!!
 
-//4998 
-
-//5739
-
-// let startAnimation = window.scrollY;
-// let endAnimation = window.scrollY;
-
-
-
-// addEventListener('DOMContentLoaded', function scrollPage() {
-//     if (startAnimation >= 4998 && startAnimation <= 5739) {
-//         return alert('Parabéns');
-//     }
-// })
-
-// console.log(elementsImg);
-//4996 5996
-//4339 5339
-//5072 and 6128 --> 1056
-
 //A função abaixo já funciona, porém deve ser ajustada a fim de funcionar para diferentes tamanhos de tela. Nesse caso, devemos pegar a altura da <section>> ao invés da quantidade px rolada.
 
 // let scrolled;
@@ -125,27 +105,89 @@ function transformSpanAuto() {
 // }, false); 
 
 
+// window.addEventListener("scroll", function (event) {
+
+//     let top = this.scrollY;
+//     let figmaisProSection = document.querySelector('.modules').offsetTop;
+//     let welcomeToFigmaisProSection = document.querySelector('.welcometofigmaispro').offsetTop;
+//     let animation = top - figmaisProSection;
+//     let sectionHeight = welcomeToFigmaisProSection - figmaisProSection
+
+//     let elementsImg = document.querySelector('.elementsimg')
+//     let elementsimgValueToMove = 3.5 / sectionHeight;
+
+//     if (top >= figmaisProSection && top <= welcomeToFigmaisProSection) {
+//         elementsImg.style.transform = 'translateY(' + (animation * elementsimgValueToMove) + 'rem';
+//     };
+
+//     let resourcesImg = document.querySelector('.resourcesimg')
+//     let resourcesImgValueToMove = 4.5 / sectionHeight;
+
+//     if (top >= figmaisProSection && top <= welcomeToFigmaisProSection) {
+//         resourcesImg.style.transform = 'translateX(' + (animation * resourcesImgValueToMove) + 'rem';
+//     };
+
+//     let classesImg = document.querySelector('.classesimg')
+//     let classesImgValueToMove = 3 / sectionHeight;
+
+//     if (top >= figmaisProSection && top <= welcomeToFigmaisProSection) {
+//         classesImg.style.transform = 'translateX(' + (-animation * classesImgValueToMove) + 'rem';
+//     };
+
+//     let modulesImg = document.querySelector('.modulesimg')
+//     let modulesImgValueToMove = 3 / sectionHeight;
+
+//     if (top >= figmaisProSection && top <= welcomeToFigmaisProSection) {
+//         modulesImg.style.transform = 'translateX(' + (animation * modulesImgValueToMove) + 'rem';
+//     };
+
+//     let studentsImg = document.querySelector('.studentsimg')
+//     let studentsImgValueToMove = 2.5 / sectionHeight;
+
+//     if (top >= figmaisProSection && top <= welcomeToFigmaisProSection) {
+//         studentsImg.style.transform = 'translateX(' + (-animation * studentsImgValueToMove) + 'rem';
+//     };
+
+//     let updatesImg = document.querySelector('.updatesimg')
+//     let updatesImgValueToMove = 3.5 / sectionHeight;
+
+//     if (top >= figmaisProSection && top <= welcomeToFigmaisProSection) {
+//         updatesImg.style.transform = 'translateY(' + (-animation * updatesImgValueToMove) + 'rem';
+//     };
+// }, false);
+
 window.addEventListener("scroll", function (event) {
 
-    let topRef = this.document.getElementById('startAnimation');
-    let bottomRef = this.document.getElementById('endAnimation');
-    
-
-    // let parentTop = topRef.offsetTop;
-    let parentBottom = bottomRef.offsetTop;
-
-    let elementsImg = document.querySelector('.elementsimg')
-
-    // let top = this.scrollY;
     let top = this.scrollY;
+    let start = document.querySelector('.modules').offsetTop;
+    let end = document.querySelector('.welcometofigmaispro').offsetTop;
+    let animation = top - start;
+    let sectionHeight = end - start;
 
-    // console.log(parentTop, parentBottom);
+    let elementsImg = document.querySelector('.elementsimg');
+    let elementsimgValueToMove = 3.5 / sectionHeight;
 
-    let animation = parentBottom - top;
+    let resourcesImg = document.querySelector('.resourcesimg')
+    let resourcesImgValueToMove = 4.5 / sectionHeight;
 
-    var valueToMove = 4 / (parentBottom - top);
+    let classesImg = document.querySelector('.classesimg')
+    let classesImgValueToMove = 3 / sectionHeight;
 
-    if (animation > 0 && animation <= 707) {
-        elementsImg.style.transform = 'translateY(' + (animation * valueToMove) + 'rem';
-    }
-}, false); 
+    let modulesImg = document.querySelector('.modulesimg')
+    let modulesImgValueToMove = 3 / sectionHeight;
+
+    let studentsImg = document.querySelector('.studentsimg')
+    let studentsImgValueToMove = 2.5 / sectionHeight;
+
+    let updatesImg = document.querySelector('.updatesimg')
+    let updatesImgValueToMove = 3.5 / sectionHeight;
+
+    if (top >= start && top <= end) {
+        elementsImg.style.transform = 'translateY(' + (animation * elementsimgValueToMove) + 'rem';
+        resourcesImg.style.transform = 'translateX(' + (animation * resourcesImgValueToMove) + 'rem';
+        classesImg.style.transform = 'translateX(' + (-animation * classesImgValueToMove) + 'rem';
+        modulesImg.style.transform = 'translateX(' + (animation * modulesImgValueToMove) + 'rem';
+        studentsImg.style.transform = 'translateX(' + (-animation * studentsImgValueToMove) + 'rem';
+        updatesImg.style.transform = 'translateY(' + (-animation * updatesImgValueToMove) + 'rem';
+    };
+}, false);
